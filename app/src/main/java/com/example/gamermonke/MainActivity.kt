74e4f2e311966cb4dd19c30e853f5c6eed634664
KDwyer18 <72669832+KDwyer18@users.noreply.github.com>
2023-02-08 16:11:12 -0700

    basic ui

--- app/src/main/java/com/example/gamermonke/MainActivity.kt
+++ app/src/main/java/com/example/gamermonke/MainActivity.kt
@@ -5,32 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.gamermonke.databinding.ActivityMainBinding
+import com.example.gamermonke.databinding.ActivityMainBinding.*
 
-private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener {
-            openRegister()
-        }
-//        openRegister()
-        replaceFragment(Home())
+        openRegister()
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
 
-            when(it.itemId){
-                R.id.home -> replaceFragment(Home())
-                R.id.bmr -> replaceFragment(BMR())
-                R.id.tbd -> replaceFragment(TBD())
-
-                else ->{}
-            }
-
-            true
-        }
 
     }
 
@@ -39,13 +26,6 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun replaceFragment(fragment : Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-        fragmentTransaction.commit()
 
-
-    }
 
 }
